87de035476b27cd2df6b22b850b82e249916ae02
derici <91606809+derici@users.noreply.github.com>
2022-08-26 03:37:48 +0300

    Add files via upload

---
+++ PropertiesAssemblyInfo.cs
@@ -0,0 +1,15 @@
+[assembly: AssemblyTitle("Занималка ников by Сука")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Сука ™")]
+[assembly: AssemblyProduct("Занималка ников by Сука")]
+[assembly: AssemblyCopyright("Copyright Сука©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a56a3272-6999-46bc-abe3-0a5512ed9c9e")]
+
+[assembly: AssemblyVersion("1")]
+[assembly: AssemblyFileVersion("1")]
